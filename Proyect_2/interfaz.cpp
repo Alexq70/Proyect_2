55a774bb996d14351985ff706b12ad0a06533c36
jbonilla0204 <jarod.bonilla.granados@est.una.ac.cr>
2025-06-04 02:02:28 -0600

    interfaz

--- Proyect_2/interfaz.cpp
+++ Proyect_2/interfaz.cpp
@@ -17,13 +17,8 @@ interfaz::interfaz()
 
 void interfaz::mostrar()
 {
-	elemento* p2 = new planta('P', "2", coordenada(6,10));
-	elemento* p3 = new planta('P', "3", coordenada(4,8));
-	elemento* a1 = new agua('A', "4", coordenada(8,12));
 
 	elemento* c2 = new carnivoro('C', "2", 98, 34, coordenada(4, 3));
-	elemento* c3 = new herbivoro('H', "3", 56, 33, coordenada(7, 14));
-	elemento* c4 = new omnivoro('O', "4", 44, 23, coordenada(1, 3));
 
 
 	coleccionVector<elemento*>* elementos = new coleccionVector<elemento*>();
