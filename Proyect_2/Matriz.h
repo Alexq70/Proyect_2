b094bb771b8b35f71988f9dbd34e894d3464719d
jbonilla0204 <jarod.bonilla.granados@est.una.ac.cr>
2025-06-04 02:57:56 -0600

    liata natriz y colecciones ademas de haber arreglado toda la logica de la herencia

--- Proyect_2/Matriz.h
+++ Proyect_2/Matriz.h
@@ -3,17 +3,18 @@
 #define MATRIZ_H
 #include "iteradorVector.h"
 #include "coleccionVector.h"
+#include "coordenada.h"
 #include "elemento.h"
 #include <iostream>
 #include <sstream>
 using namespace std;
 class matriz {
 private:
-	iteradorVector<elemento>* iterador;
-	elemento* m[20][20];
+	iteradorVector<elemento*>* iterador;
+	elemento* m[10][15];
 public:
 	matriz();
-	matriz(coleccionVector <elemento>*);
+	matriz(iteradorVector<elemento*>*);
 	bool insertarElemneto(elemento* e, int x, int y);
 	bool eliminarElemento(int x, int y);
 	string mostrarMatriz();
