0268d81be7b1fd931be5a729611adb17a5c842ff
jbonilla0204 <jarod.bonilla.granados@est.una.ac.cr>
2025-06-04 02:05:17 -0600

    matriz.cpp

--- Proyect_2/Matriz.cpp
+++ Proyect_2/Matriz.cpp
@@ -31,6 +31,7 @@ bool matriz::insertarElemneto(elemento* e, int x, int y)
 		m[x][y] = e;
 	}
 	else {
+		;
 		return false; // Si la posicion ya esta ocupada, no se inserta el elemento
 	}
 	return true;
