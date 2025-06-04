7159650b08803f381e64fedef13d68f12ac7a001
jbonilla0204 <jarod.bonilla.granados@est.una.ac.cr>
2025-06-04 02:06:00 -0600

    matriz.h

--- Proyect_2/Matriz.h
+++ Proyect_2/Matriz.h
@@ -1,13 +1,22 @@
 #pragma once
 #ifndef MATRIZ_H
 #define MATRIZ_H
+#include "iteradorVector.h"
+#include "coleccionVector.h"
+#include "elemento.h"
 #include <iostream>
 #include <sstream>
 using namespace std;
 class matriz {
 private:
-	
+	iteradorVector<elemento>* iterador;
+	elemento* m[20][20];
 public:
 	matriz();
+	matriz(coleccionVector <elemento>*);
+	bool insertarElemneto(elemento* e, int x, int y);
+	bool eliminarElemento(int x, int y);
+	string mostrarMatriz();
+};
 #endif 
 
