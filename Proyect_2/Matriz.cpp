aadc8857162753854e538a87665ee1d94c7b0948
jbonilla0204 <jarod.bonilla.granados@est.una.ac.cr>
2025-06-04 02:57:56 -0600

    liata natriz y colecciones ademas de haber arreglado toda la logica de la herencia

--- Proyect_2/Matriz.cpp
+++ Proyect_2/Matriz.cpp
@@ -3,12 +3,23 @@
 matriz::matriz()
 {
 	this->iterador = nullptr;
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 15; j++) {
 			m[i][j] = nullptr; // Inicializar la matriz con punteros nulos
 		}
 	}
 }
 
+matriz::matriz(iteradorVector<elemento*>* c)
 {
+	this->iterador = c;
+	c->first();
+	while (!c->isDone()) {
+		elemento* e = *c->current();
+		int x = e->getCoordenada().getX();
+		int y = e->getCoordenada().getY();
+		if (x >= 0 && x < 10 && y >= 0 && y < 15) { // Verificar que las coordenadas estén dentro del rango
+			m[x][y] = e; // Insertar el elemento en la matriz
 		}
 		c->next();
 	}
@@ -20,7 +31,6 @@ bool matriz::insertarElemneto(elemento* e, int x, int y)
 		m[x][y] = e;
 	}
 	else {
-		;
 		return false; // Si la posicion ya esta ocupada, no se inserta el elemento
 	}
 	return true;
@@ -41,7 +51,16 @@ bool matriz::eliminarElemento(int x, int y)
 string matriz::mostrarMatriz()
 {
 	stringstream ss;
+	for (int i = 0; i < 10; i++) {
+		if (i > 0) {
+			ss << endl; // Nueva línea entre filas
+		}
+		for (int j = 0; j < 15; j++) {
 			if (m[i][j] != nullptr) {
+				ss << m[i][j]->getTipo();
+			}
+			else {
+				ss << ".";
 			}
 		}
 	}
--- Proyect_2/interfaz.cpp
+++ Proyect_2/interfaz.cpp
@@ -12,13 +12,18 @@
 
 interfaz::interfaz()
 {
-	this->mostrar();
+	this->comenzar();
 }
 
-void interfaz::mostrar()
+void interfaz::comenzar()
 {
+	elemento* p2 = new planta('P', "2", coordenada(1, 2));
+	elemento* p3 = new planta('P', "3", coordenada(1, 2));
+	elemento* a1 = new agua('A', "4", coordenada(1, 2));
 
 	elemento* c2 = new carnivoro('C', "2", 98, 34, coordenada(4, 3));
+	elemento* c3 = new herbivoro('H', "3", 56, 33, coordenada(1, 2));
+	elemento* c4 = new omnivoro('O', "4", 44, 23, coordenada(1, 2));
 
 
 	coleccionVector<elemento*>* elementos = new coleccionVector<elemento*>();
@@ -30,7 +35,7 @@ void interfaz::mostrar()
 	elementos->agregarObjeto(p2);
 	elementos->agregarObjeto(p3);
 	elementos->agregarObjeto(a1);
-
-
-	cout << elementos->toString();
+	matriz* m = new matriz(elementos->getIterador());
+	cout<<m->mostrarMatriz()<<endl<<endl;
+	//cout << elementos->toString();
 }
