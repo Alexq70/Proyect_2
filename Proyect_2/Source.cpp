2d895dcf88d479312e1136def07f10c6ea2f6044
Quezzada <alexquesadab7@gmail.com>
2025-06-16 14:15:25 -0600

    descripcion participantes

--- Proyect_2/Source.cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream>
-#include "interfaz.h"
-using namespace std;
-int main() {
-	interfaz* i = new interfaz();
-	return 0;
-}
---
+++ Proyect_2/main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include "interfaz.h"
+using namespace std;
+int main() {
+	interfaz* i = new interfaz();
+/* ------------------------------------+
+* (c) 2025                             |
+* EIF204 - Programación 2              |
+* 1er ciclo 2025                       |
+* NRC 21370 – Grupo 05                 |
+* Proyecto 2                           |
+* 1-1870-0702; Bonilla Granados Jarod  |
+* 2-0873-0258; Quesada Bermudez Alex   |
+* versión 1.0.0 2025-06-16             | 
+* -------------------------------------+
+*/
+	return 0;
+}
